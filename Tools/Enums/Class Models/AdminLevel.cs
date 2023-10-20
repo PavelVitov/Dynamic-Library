5a55e9cf641226a52f2cb5dad780c75a5df6f7cf
Pavel Vitov <ivov.vitov@gmail.com>
2023-10-20 13:19:25 +0300

    Enum Created

---
+++ Tools/Enums/Class Models/AdminLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Enums.Class_Models
+{
+    public enum AdminLevel
+    {
+        Level1,
+        Level2, 
+        Level3
+    }
+}
---
+++ Tools/Enums/Class Models/Gene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Enums.Class_Models
+{
+    public enum Gene
+    {
+        None,
+        Gaming,
+        Gambling,
+        Ethical,
+        Friendly,
+        Charecters
+    }
+}
