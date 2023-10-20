ce884088468a4673115252eb43e4205b4813dad6
Pavel Vitov <ivov.vitov@gmail.com>
2023-10-20 13:31:48 +0300

    DbContext Add

--- Data Layer/DataBase/LibraryDbContext.cs
+++ Data Layer/DataBase/LibraryDbContext.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tools.Models.Class_Models;
 
 namespace Data_Layer.DataBase
 {
-    public class LibraryDbContext
+    public class LibraryDbContext : DbContext
     {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) 
+            : base(options) { }
 
+        public DbSet<Choice> Choices { get; set; }
+        public DbSet<Poll> Polls { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LibraryDbContext).Assembly);
+        }
     }
 }
